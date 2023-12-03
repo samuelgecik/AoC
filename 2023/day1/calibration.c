183922592bf9ed3ff20203cadaa16cae263ac27f
Samuel Gecik <68155940+samuelgecik@users.noreply.github.com>
2023-12-03 16:14:05 +0100

    Optimize code
    Only do one run

--- 2023/day1/calibration.c
+++ 2023/day1/calibration.c
@@ -12,27 +12,23 @@ int main() {
     return -1;
   }
 
-  int first = 0;
-  int last = 0;
+  int first = -1;
+  int last = -1;
   int digit = 0;
 
   while (fgets(line, MAX_LENGTH, fp) != NULL) {
     int i = 0;
     while (line[i] != '\0') {
       if (isdigit(line[i])) {
-        first = line[i] - '0';
-        break;
-      }
-      i++;
-    }
-    i = 0;
-    while (line[i] != '\0') {
-      if (isdigit(line[i])) {
+        if (first < 0) {
+          first = line[i] - '0';
+        }
         last = line[i] - '0';
       }
       i++;
     }
     digit += 10 * first + last;
+    first = -1;
   }
 
   fclose(fp);
