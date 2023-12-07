4645190a447654af6b36479e145e0bf4ff264d56
Samuel Gecik <68155940+samuelgecik@users.noreply.github.com>
2023-12-07 23:38:26 +0100

    Finish first part

--- 2023/day2/game.c
+++ 2023/day2/game.c
@@ -5,10 +5,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+struct game {
   int id;
+  int nrounds;
   int **rounds;
-} game;
+  bool is_possible;
+};
 
 #define AR_LENGTH 3
 #define MAX_LINE_LENGTH 256
@@ -40,27 +42,20 @@ char *find_next_num(char *input) {
   return input;
 }
 
-int main() {
-  FILE *fp = fopen(INPUT, "r");
-  if (!fp) {
-    perror("File not opened!");
-    return -1;
-  }
+void parse_input(struct game *games, FILE *fp) {
   char line[MAX_LINE_LENGTH];
-  game games[100];
-
   for (size_t i = 0; i < NUM_GAMES; i++) {
     fgets(line, MAX_LINE_LENGTH, fp);
     char cp_line[strlen(line) + 1];
     strcpy(cp_line, line);
-    int nrounds = get_nrounds(cp_line);
+    games[i].nrounds = get_nrounds(cp_line);
     char *color = strtok(line, ":");
     char *end;
     int id = (int)strtol(color + 5, &end, 10);
     color = strtok(NULL, ";");
     games[i].id = id;
-    games[i].rounds = calloc(nrounds, sizeof(int *));
-    for (size_t j = 0; j < nrounds; j++) {
+    games[i].rounds = calloc(games[i].nrounds, sizeof(int *));
+    for (size_t j = 0; j < games[i].nrounds; j++) {
       games[i].rounds[j] = calloc(3, sizeof(int));
       int n = 0;
       while (n < 3 && color) {
@@ -77,11 +72,39 @@ int main() {
         }
         n++;
       }
-      printf("Round %d: %d, %d, %d\n", j,
-             games[i].rounds[j][0], games[i].rounds[j][1],
-             games[i].rounds[j][2]);
+      printf("Round %d: %d, %d, %d\n", j, games[i].rounds[j][0],
+             games[i].rounds[j][1], games[i].rounds[j][2]);
       color = strtok(NULL, ";");
     }
   }
+}
+
+int main() {
+  FILE *fp = fopen(INPUT, "r");
+  if (!fp) {
+    perror("File not opened!");
+    return -1;
+  }
+  struct game games[100];
+  parse_input(games, fp);
+  int nred, ngreen, nblue;
+  printf("Please input your values: ");
+  scanf("%d %d %d", &nred, &ngreen, &nblue);
+  int id_sum = 0;
+  for (int i = 0; i < 100; i++) {
+    games[i].is_possible = true;
+    for (size_t j = 0; j < games[i].nrounds; j++) {
+      int *colors = games[i].rounds[j];
+      if (colors[0] > nred || colors[1] > ngreen || colors[2] > nblue) {
+        games[i].is_possible = false;
+      }
+      free(games[i].rounds[j]);
+    }
+    if (games[i].is_possible) {
+      id_sum += games[i].id;
+    }
+    free(games[i].rounds);
+  }
+  printf("\nID sum: %d\n", id_sum);
   fclose(fp);
 }
